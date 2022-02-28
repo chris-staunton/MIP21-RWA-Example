3658f9fba3efe52c3ac4c7169d81e715d58a41f9
Chris Staunton <stauntch@tcd.ie>
2022-02-28 17:53:34 +0000

    added checks for ratio - WIP

--- src/RwaLiquidationOracle.sol
+++ src/RwaLiquidationOracle.sol
@@ -1,3 +1,11 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-03
+*/
+
+/**
+ *Submitted for verification at Etherscan.io on 2021-03-02
+*/
+
 // Copyright (C) 2020, 2021 Lev Livnev <lev@liv.nev.org.uk>
 //
 // This program is free software: you can redistribute it and/or modify
@@ -15,8 +23,220 @@
 
 pragma solidity >=0.5.12;
 
-import "dss-interfaces/dss/VatAbstract.sol";
-import "ds-value/value.sol";
+
+interface VatAbstract {
+    function wards(address) external view returns (uint256);
+    function rely(address) external;
+    function deny(address) external;
+    function can(address, address) external view returns (uint256);
+    function hope(address) external;
+    function nope(address) external;
+    function ilks(bytes32) external view returns (uint256, uint256, uint256, uint256, uint256);
+    function urns(bytes32, address) external view returns (uint256, uint256);
+    function gem(bytes32, address) external view returns (uint256);
+    function dai(address) external view returns (uint256);
+    function sin(address) external view returns (uint256);
+    function debt() external view returns (uint256);
+    function vice() external view returns (uint256);
+    function Line() external view returns (uint256);
+    function live() external view returns (uint256);
+    function init(bytes32) external;
+    function file(bytes32, uint256) external;
+    function file(bytes32, bytes32, uint256) external;
+    function cage() external;
+    function slip(bytes32, address, int256) external;
+    function flux(bytes32, address, address, uint256) external;
+    function move(address, address, uint256) external;
+    function frob(bytes32, address, address, address, int256, int256) external;
+    function fork(bytes32, address, address, int256, int256) external;
+    function grab(bytes32, address, address, address, int256, int256) external;
+    function heal(uint256) external;
+    function suck(address, address, uint256) external;
+    function fold(bytes32, address, int256) external;
+}
+
+interface DSAuthority {
+    function canCall(
+        address src, address dst, bytes4 sig
+    ) external view returns (bool);
+}
+
+contract DSAuthEvents {
+    event LogSetAuthority (address indexed authority);
+    event LogSetOwner     (address indexed owner);
+}
+
+contract DSAuth is DSAuthEvents {
+    DSAuthority  public  authority;
+    address      public  owner;
+
+    constructor() public {
+        owner = msg.sender;
+        emit LogSetOwner(msg.sender);
+    }
+
+    function setOwner(address owner_)
+        public
+        auth
+    {
+        owner = owner_;
+        emit LogSetOwner(owner);
+    }
+
+    function setAuthority(DSAuthority authority_)
+        public
+        auth
+    {
+        authority = authority_;
+        emit LogSetAuthority(address(authority));
+    }
+
+    modifier auth {
+        require(isAuthorized(msg.sender, msg.sig), "ds-auth-unauthorized");
+        _;
+    }
+
+    function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
+        if (src == address(this)) {
+            return true;
+        } else if (src == owner) {
+            return true;
+        } else if (authority == DSAuthority(0)) {
+            return false;
+        } else {
+            return authority.canCall(src, address(this), sig);
+        }
+    }
+}
+
+pragma solidity >=0.4.23;
+
+contract DSNote {
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  guy,
+        bytes32  indexed  foo,
+        bytes32  indexed  bar,
+        uint256           wad,
+        bytes             fax
+    ) anonymous;
+
+    modifier note {
+        bytes32 foo;
+        bytes32 bar;
+        uint256 wad;
+
+        assembly {
+            foo := calldataload(4)
+            bar := calldataload(36)
+            wad := callvalue()
+        }
+
+        _;
+
+        emit LogNote(msg.sig, msg.sender, foo, bar, wad, msg.data);
+    }
+}
+
+contract DSMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, "ds-math-add-overflow");
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    function min(uint x, uint y) internal pure returns (uint z) {
+        return x <= y ? x : y;
+    }
+    function max(uint x, uint y) internal pure returns (uint z) {
+        return x >= y ? x : y;
+    }
+    function imin(int x, int y) internal pure returns (int z) {
+        return x <= y ? x : y;
+    }
+    function imax(int x, int y) internal pure returns (int z) {
+        return x >= y ? x : y;
+    }
+
+    uint constant WAD = 10 ** 18;
+    uint constant RAY = 10 ** 27;
+
+    //rounds to zero if x*y < WAD / 2
+    function wmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), WAD / 2) / WAD;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+    //rounds to zero if x*y < WAD / 2
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, WAD), y / 2) / y;
+    }
+    //rounds to zero if x*y < RAY / 2
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    // This famous algorithm is called "exponentiation by squaring"
+    // and calculates x^n with x as fixed-point and n as regular unsigned.
+    //
+    // It's O(log n), instead of O(n) for naive repeated multiplication.
+    //
+    // These facts are why it works:
+    //
+    //  If n is even, then x^n = (x^2)^(n/2).
+    //  If n is odd,  then x^n = x * x^(n-1),
+    //   and applying the equation for even x gives
+    //    x^n = x * (x^2)^((n-1) / 2).
+    //
+    //  Also, EVM division is flooring and
+    //    floor[(n-1) / 2] = floor[n / 2].
+    //
+    function rpow(uint x, uint n) internal pure returns (uint z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
+}
+
+contract DSThing is DSAuth, DSNote, DSMath {
+    function S(string memory s) internal pure returns (bytes4) {
+        return bytes4(keccak256(abi.encodePacked(s)));
+    }
+
+}
+
+contract DSValue is DSThing {
+    bool    has;
+    bytes32 val;
+    function peek() public view returns (bytes32, bool) {
+        return (val,has);
+    }
+    function read() public view returns (bytes32) {
+        bytes32 wut; bool haz;
+        (wut, haz) = peek();
+        require(haz, "haz-not");
+        return wut;
+    }
+    function poke(bytes32 wut) public note auth {
+        val = wut;
+        has = true;
+    }
+    function void() public note auth {  // unset the value
+        has = false;
+    }
+}
 
 contract RwaLiquidationOracle {
     // --- auth ---
@@ -35,11 +255,16 @@ contract RwaLiquidationOracle {
     }
 
     // --- math ---
-    function add(uint48 x, uint48 y) internal pure returns (uint48 z) {
+    function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x);
     }
     function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require(y == 0 || (z = x * y) / y == x);
+    }
+    // uint constant WAD = 10 ** 18;
+    //rounds to zero if x*y < WAD / 2
+    function div(uint256 x, uint256 y) internal pure returns (uint48 z) {
+        z = uint48((x*100)/ y);
     }
 
     VatAbstract public vat;
@@ -49,6 +274,8 @@ contract RwaLiquidationOracle {
         address pip; // DSValue tracking nominal loan value
         uint48  tau; // pre-agreed remediation period
         uint48  toc; // timestamp when liquidation initiated
+        address urn; // urn of ilk
+        uint48 ratio; // collateral ratio
     }
     mapping (bytes32 => Ilk) public ilks;
 
@@ -58,6 +285,7 @@ contract RwaLiquidationOracle {
     event File(bytes32 indexed what, address data);
     event Init(bytes32 indexed ilk, uint256 val, string doc, uint48 tau);
     event Bump(bytes32 indexed ilk, uint256 val);
+    event Dip(bytes32 indexed ilk, uint256 val);
     event Tell(bytes32 indexed ilk);
     event Cure(bytes32 indexed ilk);
     event Cull(bytes32 indexed ilk, address indexed urn);
@@ -77,11 +305,17 @@ contract RwaLiquidationOracle {
         emit File(what, data);
     }
 
-    function init(bytes32 ilk, uint256 val, string calldata doc, uint48 tau) external auth {
+    function init(bytes32 ilk, uint256 val, string calldata doc, uint48 tau, address urn) external auth {
         // doc, and tau can be amended, but tau cannot decrease
         require(tau >= ilks[ilk].tau, "RwaOracle/decreasing-tau");
         ilks[ilk].doc = doc;
         ilks[ilk].tau = tau;
+        ilks[ilk].urn = urn;
+        // (,,,uint256 line,) = vat.ilks(ilk);
+
+        ilks[ilk].ratio = div(val,20000000000000000000000000);
+        
+
         if (ilks[ilk].pip == address(0)) {
             DSValue pip = new DSValue();
             ilks[ilk].pip = address(pip);
@@ -109,9 +343,12 @@ contract RwaLiquidationOracle {
         require(ilks[ilk].toc == 0, "RwaOracle/in-remediation");
         // only bump can increase
         require(val <= uint256(pip.read()), "RwaOracle/increasing-val");
+        //val cannot be lower than line (DC)
+        (,,,uint256 line,) = vat.ilks(ilk);
+        require(val>=line, "RwaOracle/line-breached");
         pip.poke(bytes32(val));
 
-        emit Bump(ilk, val);
+        emit Dip(ilk, val);
     }
 
     // --- liquidation ---
@@ -131,6 +368,7 @@ contract RwaLiquidationOracle {
         emit Cure(ilk);
     }
     // --- write-off ---
+    maybe not auth -> can be called by anyone
     function cull(bytes32 ilk, address urn) external auth {
         require(ilks[ilk].pip != address(0), "RwaOracle/unknown-ilk");
         require(block.timestamp >= add(ilks[ilk].toc, ilks[ilk].tau), "RwaOracle/early-cull");
@@ -154,9 +392,22 @@ contract RwaLiquidationOracle {
     // to be called by off-chain parties (e.g. a trustee) to check the standing of the loan
     function good(bytes32 ilk) external view returns (bool) {
         require(ilks[ilk].pip != address(0), "RwaOracle/unknown-ilk");
-        ilks[ilk].val;
-        (,,) = vat.ilks(ilk);
+        // address urn = ilks[ilk].urn;
+        (uint256 art,,,,) = vat.ilks(ilk);
+        uint val = uint256(DSValue(ilks[ilk].pip).read());
+        //if debt is 0 then dont check ratio
+        if(art==0){
+            return true;
+        }
+        //else get the current collat ratio
+        uint newRatio = div(val,art);
+        if(newRatio>=ilks[ilk].ratio){
+            return true;
+        }
+        else{
+            return false;
+        }
         // tell not called or still in remediation period
-        return (ilks[ilk].toc == 0 || block.timestamp < add(ilks[ilk].toc, ilks[ilk].tau));
+        // return (ilks[ilk].toc == 0 || block.timestamp < add(ilks[ilk].toc, ilks[ilk].tau));
     }
 }
