08bb7606e425fc53e925ee9ebdd1c4e88d87a73a
Chris Staunton <stauntch@tcd.ie>
2022-04-02 19:19:27 +0100

    added nft ability & cleanup

--- src/RwaUrn.sol
+++ src/RwaUrn.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2021-03-02
-*/
-
 // Copyright (C) 2020, 2021 Lev Livnev <lev@liv.nev.org.uk>
 //
 // This program is free software: you can redistribute it and/or modify
@@ -17,7 +13,22 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity 0.5.12;
+pragma solidity >=0.5.12;
+
+
+interface ERC721 {
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+    function balanceOf(address _owner) external view returns (uint256);
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+    function approve(address _approved, uint256 _tokenId) external payable;
+    function setApprovalForAll(address _operator, bool _approved) external;
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+}
 
 // https://github.com/makerdao/dss/blob/master/src/spot.sol
 interface SpotAbstract {
@@ -120,8 +131,8 @@ interface GemJoinAbstract {
     function dec() external view returns (uint256);
     function live() external view returns (uint256);
     function cage() external;
-    function join(address, uint256) external;
-    function exit(address, uint256) external;
+    function join(address, bytes32, uint256) external;
+    function exit(address, bytes32, uint256) external;
 }
 
 // https://github.com/makerdao/dss/blob/master/src/join.sol
@@ -163,7 +174,11 @@ interface DaiAbstract {
     function permit(address, address, uint256, uint256, bool, uint8, bytes32, bytes32) external;
 }
 
-contract RwaUrn {
+interface ERC721TokenReceiver {
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data) external returns(bytes4);
+}
+
+contract RwaUrn is ERC721TokenReceiver{
     // --- auth ---
     mapping (address => uint256) public wards;
     mapping (address => uint256) public can;
@@ -192,12 +207,13 @@ contract RwaUrn {
         _;
     }
 
-    VatAbstract  public vat;
-    JugAbstract  public jug;
-    GemJoinAbstract public gemJoin;
-    DaiJoinAbstract public daiJoin;
-    address public outputConduit;
-    SpotAbstract public spot;
+    VatAbstract  public vat;            // MCD vat
+    JugAbstract  public jug;            // MCD jug
+    GemJoinAbstract public gemJoin;     // NFT adapter
+    DaiJoinAbstract public daiJoin;     // DAI adapter
+    address public recipient;           // address to recieve DAI
+    uint256 public tokenId;             // NFT token identifier
+    bytes32 public ilk;                 // collateral name
 
     // Events
     event Rely(address indexed usr);
@@ -213,6 +229,7 @@ contract RwaUrn {
 
     // --- math ---
     uint256 constant RAY = 10 ** 27;
+    uint256 constant WAD = 10 ** 18;
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require((z = x + y) >= x);
     }
@@ -228,27 +245,38 @@ contract RwaUrn {
 
     // --- init ---
     constructor(
-        address vat_, address jug_, address gemJoin_, address daiJoin_, address outputConduit_, address spot_
+        address vat_, address jug_, address gemJoin_, address daiJoin_, address recipient_, uint256 tokenId_, bytes32 ilk_
     ) public {
         // requires in urn that outputConduit isn't address(0)
+        require(recipient_ != address(0),"RwaUrn/invalid-recipient");
+        //set contract addresses
         vat = VatAbstract(vat_);
         jug = JugAbstract(jug_);
         gemJoin = GemJoinAbstract(gemJoin_);
         daiJoin = DaiJoinAbstract(daiJoin_);
-        outputConduit = outputConduit_;
-        spot = SpotAbstract(spot_);
+        recipient = recipient_;
+        //approve creator
         wards[msg.sender] = 1;
-        DSTokenAbstract(gemJoin.gem()).approve(address(gemJoin), uint256(-1));
-        DaiAbstract(daiJoin.dai()).approve(address(daiJoin), uint256(-1));
+        //has to be one less to fit in uint256
+        uint256 maxVal = sub((2**255),1);
+        //approve daiJoin
+        DaiAbstract(daiJoin.dai()).approve(address(daiJoin), maxVal);
         VatAbstract(vat_).hope(address(daiJoin));
+        //approve nft transfer
+        ERC721(gemJoin.gem()).approve(address(this), tokenId);
+
+        //set tokenID i.e. identify nft associated with this vault
+        tokenId = tokenId_;
+        ilk = ilk_;
+
         emit Rely(msg.sender);
-        emit File("outputConduit", outputConduit_);
+        emit File("outputConduit", recipient_);
         emit File("jug", jug_);
     }
 
     // --- administration ---
     function file(bytes32 what, address data) external auth {
-        if (what == "outputConduit") { outputConduit = data; }
+        if (what == "recipient") { recipient = data; }
         else if (what == "jug") { jug = JugAbstract(data); }
         else revert("RwaUrn/unrecognised-param");
         emit File(what, data);
@@ -256,56 +284,95 @@ contract RwaUrn {
 
     // --- cdp operation ---
     // n.b. that the operator must bring the gem
-    function lock(uint256 wad) external operator {
-        require(wad <= 2**255 - 1, "RwaUrn/overflow");
-        DSTokenAbstract(gemJoin.gem()).transferFrom(msg.sender, address(this), wad);
-        // join with address this
-        gemJoin.join(address(this), wad);
-        vat.frob(gemJoin.ilk(), address(this), address(this), address(this), int(wad), 0);
-        emit Lock(msg.sender, wad);
+    function lock() external operator {
+        //send nft to thisUrn
+        ERC721(gemJoin.gem()).safeTransferFrom(msg.sender, address(this), tokenId);
+        //approve NftAdapter
+        ERC721(gemJoin.gem()).approve(address(gemJoin), tokenId);
+        //send nft to nftAdapter
+        gemJoin.join(address(this), ilk, tokenId);
+        //increase vault collateral balance by 1
+        vat.frob(ilk, address(this), address(this), address(this), int(WAD), 0);
+        emit Lock(msg.sender, WAD);
     }
     // n.b. that the operator takes the gem
     // and might not be the same operator who brought the gem
-    function free(uint256 wad) external operator {
-        require(wad <= 2**255, "RwaUrn/overflow");
-        vat.frob(gemJoin.ilk(), address(this), address(this), address(this), -int(wad), 0);
-        gemJoin.exit(msg.sender, wad);
-        emit Free(msg.sender, wad);
+    function free() external operator {
+        //decrease collateral balance by 1
+        vat.frob(ilk, address(this), address(this), address(this), -int(WAD), 0);
+        //send nft to operator
+        gemJoin.exit(msg.sender, ilk, tokenId);
+        emit Free(msg.sender, WAD);
     }
     // n.b. DAI can only go to the output conduit
     function draw(uint256 wad) external operator {
-        require(outputConduit != address(0));
-        bytes32 ilk = gemJoin.ilk();
+        require(recipient != address(0));
+        //update fees
         jug.drip(ilk);
-        (uint256 Art,uint256 rate,,,) = vat.ilks(ilk);
+        //get normalised debt
+        (,uint256 rate,,,) = vat.ilks(ilk);
         uint256 dart = divup(mul(RAY, wad), rate);
         require(dart <= 2**255 - 1, "RwaUrn/overflow");
-        //require ratio is not breached
-        (,uint256 mat) = spot.ilks("RWA999-A");
-        require(((Art*100)/dart)>=mat, "RwaUrn/ratio-exceded");
+        //increase vault debt by normalised newDebt
         vat.frob(ilk, address(this), address(this), address(this), 0, int(dart));
-        daiJoin.exit(outputConduit, wad);
+        //send ERC20 DAI to recipient
+        daiJoin.exit(recipient, wad);
         emit Draw(msg.sender, wad);
     }
     // n.b. anyone can wipe
     function wipe(uint256 wad) external {
+        //send ERC20 DAI from sender to vault
         daiJoin.join(address(this), wad);
-        bytes32 ilk = gemJoin.ilk();
+        //update fees
         jug.drip(ilk);
+        //get normalised amount
         (,uint256 rate,,,) = vat.ilks(ilk);
         uint256 dart = mul(RAY, wad) / rate;
         require(dart <= 2 ** 255, "RwaUrn/overflow");
+        //decrease vault debt by normalised amount
         vat.frob(ilk, address(this), address(this), address(this), 0, -int(dart));
         emit Wipe(msg.sender, wad);
     }
 
+    //wipe total debt balance
+    function wipeAll() external {
+        //update fees
+        jug.drip(ilk);
+        //get outstanding vault debt
+        (uint256 Art,uint256 rate,,,) = vat.ilks(ilk);
+        uint256 totalDebt = mul(Art,rate)/RAY;
+        //turn ERC20 DAI into internal DAI from sender
+        daiJoin.join(msg.sender,totalDebt);
+        //decrease debt by normalised amount (to zero)
+        vat.frob(ilk, address(this), address(this), msg.sender, 0, -int(Art));
+    }
+
     // If Dai is sitting here after ES that should be sent back
     function quit() external {
-        require(outputConduit != address(0));
+        require(recipient != address(0));
         require(vat.live() == 0, "RwaUrn/vat-still-live");
         DSTokenAbstract dai = DSTokenAbstract(daiJoin.dai());
         uint256 wad = dai.balanceOf(address(this));
-        dai.transfer(outputConduit, wad);
+        dai.transfer(recipient, wad);
         emit Quit(msg.sender, wad);
+    }
+
+    //function to allow smart contract to recieve ERC721
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes calldata _data
+    )
+      external
+        override
+        returns(bytes4)
+    {
+        _operator;
+        _from;
+        _tokenId;
+        _data;
+        // emit Received();
+        return 0x150b7a02;
     }
 }
